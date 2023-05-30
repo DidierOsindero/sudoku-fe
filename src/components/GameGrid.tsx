c1c0160a256c783a3ff0491e7cd7f0138f4eb3a8
Didier Osindero <didierosindero@gmail.com>
2023-05-30 15:04:48 +0100

    Grid with input not working

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,21 +1,36 @@
 import "./GameGrid.css";
 interface IGameGridProps {
   gameGrid: string[][];
+  setGameGrid: React.Dispatch<React.SetStateAction<string[][]>>;
 }
 
-export default function GameGrid({ gameGrid }: IGameGridProps) {
+export default function GameGrid({ gameGrid, setGameGrid }: IGameGridProps) {
+  const updateGrid = (val: string, rowIdx: number, columnIdx: number) => {
+    const gameGridCopy = [...gameGrid];
+    gameGridCopy[rowIdx] = [...gameGrid[rowIdx]];
+    gameGridCopy[rowIdx][columnIdx] = val;
+    setGameGrid(gameGridCopy);
+  };
   return (
     <>
       <h1>Game Grid</h1>
       <div className="grid-container">
-        {gameGrid.map((row, idx) => {
+        {gameGrid.map((row, rowIdx) => {
           return (
-            <div key={idx} className="grid-row">
-              {row.map((cell, pos) => {
+            <div key={rowIdx} className="grid-row">
+              {row.map((cell, columnIdx) => {
                 return cell === "." ? (
-                  <input className="grid-cell" key={pos} maxLength={1} />
+                  <input
+                    className="grid-cell"
+                    value={gameGrid[rowIdx][columnIdx]}
+                    key={columnIdx}
+                    maxLength={1}
+                    onChange={(e) =>
+                      updateGrid(e.target.value, rowIdx, columnIdx)
+                    }
+                  />
                 ) : (
-                  <div className="grid-cell preset-num" key={pos}>
+                  <div className="grid-cell preset-num" key={columnIdx}>
                     {cell}
                   </div>
                 );
